33e43a6aacc4176803e12d364f5f94c716126ae1
SueNikki <2994456484@qq.com>
2022-07-03 21:08:51 +0800

    ddddd

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { AliveScope } from 'react-activation';
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
-import './App.css'
+import App from './pages/App';
+import './index.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
